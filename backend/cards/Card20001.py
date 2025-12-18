48ac52b36b64237117215729ff07f78330b412fb
Matthew <tatoba2351@gmail.com>
2025-12-18 11:47:56 +0800

    Update: create card 20001

---
+++ backend/cards/Card20001.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, List, Dict, Union, Callable, Any, Optional
+from card_related.card_driver import Card, Deck
+from card_related.system_driver import System
+from card_related.static_card_base import StaticCardBase, StaticSystemBase
+
+from chess_related.board import Board
+from chess_related.piece import BasePiece, KingPiece, QueenPiece, BishopPiece, KnightPiece, RookPiece, PawnPiece, NonePiece
+from chess_related.chess_utils import *
+
+from player_related.player import Player
+from controller import GameController
+
+
+from controller_related.event_controller import EventHandler
+
+class Card20001:
+    
+    def __init__(self, controller: GameController):
+        self.controller = controller
+    
+    def exec(self):
+        print("[Card 20001] Execution started: Draw 2 cards")
+        
+        self.controller.draw("friendly", 2)
+        
+        print("[Card 20001] Effect resolved successfully")
